5a28674df0302f85ac330aed83f81d7e75aa08e7
artwells <artwells>
2014-09-03 21:58:07 -0700

    changes to work in 0.9.0.0

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "job queue for meteor"
+  summary: "job queue for meteor",
+  version: "0.0.3",
+  git: "https://github.com/artwells/meteor-queue.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['livedata', 'mongo-livedata'], 'server');
   api.add_files('queue.js', 'server');
   api.add_files('lib/model.js', 'server');
@@ -11,7 +14,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('queue', 'server');
+  api.use('artwells:queue', 'server');
   api.use('tinytest', 'server');
   api.add_files('tests/server.js', 'server');
 });
